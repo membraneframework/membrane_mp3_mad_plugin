546719090813f973ed1480f2c67059cf41a59acf
wkozyra95 <wojciech.kozyra@swmansion.com>
2017-09-06 13:41:47 +0200

    bundlex linux config

--- config/config.exs
+++ config/config.exs
@@ -18,4 +18,23 @@ config :membrane_element_mad, :bundlex_lib,
         ]
       ]
     ]
-  ]
+  ],
+  linux: [
+    nif: [
+        membrane_element_mad_decoder: [
+          includes: [
+            "../membrane_common_c/c_src",
+            "./deps/membrane_common_c/c_src",
+          ],
+          sources: [
+            "decoder.c",
+          ],
+          libs: [
+          ],
+          pkg_configs: [
+            "mad"
+          ]
+        ]
+      ]
+    ]
+  
