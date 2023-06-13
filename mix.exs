a6b4bd939fb63b510a90bf58e81b3bbc12fd3afc
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2023-06-13 19:27:28 +0200

    bump to 0.16.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.MP3.MAD.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.15.0"
+  @version "0.16.0"
   @github_url "https://github.com/membraneframework/membrane_mp3_mad_plugin"
 
   def project do
@@ -55,7 +55,7 @@ defmodule Membrane.MP3.MAD.Plugin.Mixfile do
     [
       {:membrane_core, "~> 0.12.0"},
       {:membrane_mpegaudio_format, "~> 0.3"},
-      {:membrane_raw_audio_format, "~> 0.10.0"},
+      {:membrane_raw_audio_format, "~> 0.11.0"},
       {:membrane_common_c, "~> 0.15.0"},
       {:unifex, "~> 1.0"},
       {:ex_doc, "~> 0.28", only: :dev, runtime: false},
