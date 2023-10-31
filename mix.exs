922c4337e3387290ece0073bb8e207619c5e4663
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2023-10-31 15:56:50 +0100

    Prepare for releasing

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.MP3.MAD.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.17.1"
+  @version "0.17.2"
   @github_url "https://github.com/membraneframework/membrane_mp3_mad_plugin"
 
   def project do
