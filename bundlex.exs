95450cd4ab2b9b6442f902c3376a22cae1e91cc5
varsill <lukasz.kita0@gmail.com>
2023-11-21 12:48:32 +0100

    Format the code.

--- bundlex.exs
+++ bundlex.exs
@@ -16,10 +16,10 @@ defmodule Membrane.MP3.MAD.BundlexProject do
         "#{url_prefix}_linux.tar.gz"
 
       %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
-       "#{url_prefix}_macos_intel.tar.gz"
+        "#{url_prefix}_macos_intel.tar.gz"
 
       %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
-       	"#{url_prefix}_macos_arm.tar.gz"
+        "#{url_prefix}_macos_arm.tar.gz"
 
       _other ->
         nil
@@ -33,8 +33,8 @@ defmodule Membrane.MP3.MAD.BundlexProject do
         deps: [membrane_common_c: :membrane],
         sources: ["decoder.c"],
         os_deps: [
-		mad: [{:precompiled, get_mad_url()}, :pkg_config]
-		],
+          mad: [{:precompiled, get_mad_url()}, :pkg_config]
+        ],
         preprocessor: Unifex
       ]
     ]
