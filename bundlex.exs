691cb02b3262137749071fc2ad0be1ac330638c9
Noarkhh <jakub.pryc@gmail.com>
2023-12-14 09:21:04 +0100

    Start using Precompiled Dependency Provider

--- bundlex.exs
+++ bundlex.exs
@@ -7,25 +7,6 @@ defmodule Membrane.MP3.MAD.BundlexProject do
     ]
   end
 
-  defp get_mad_url() do
-    url_prefix =
-      "https://github.com/membraneframework-precompiled/precompiled_mad/releases/latest/download/mad"
-
-    case Bundlex.get_target() do
-      %{os: "linux"} ->
-        "#{url_prefix}_linux.tar.gz"
-
-      %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
-        "#{url_prefix}_macos_intel.tar.gz"
-
-      %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
-        "#{url_prefix}_macos_arm.tar.gz"
-
-      _other ->
-        nil
-    end
-  end
-
   def natives() do
     [
       decoder: [
@@ -33,7 +14,10 @@ defmodule Membrane.MP3.MAD.BundlexProject do
         deps: [membrane_common_c: :membrane],
         sources: ["decoder.c"],
         os_deps: [
-          mad: [{:precompiled, get_mad_url()}, :pkg_config]
+          mad: [
+            {:precompiled, Membrane.PrecompiledDependencyProvider.get_dependency_url(:mad)},
+            :pkg_config
+          ]
         ],
         preprocessor: Unifex
       ]
