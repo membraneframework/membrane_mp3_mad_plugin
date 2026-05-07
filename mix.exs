eaf7b55efc8b7f91e0a08c940147f28c4e6dc002
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-07 17:10:05 +0200

    Relax constraints of support dependencies

--- mix.exs
+++ mix.exs
@@ -54,8 +54,7 @@ defmodule Membrane.MP3.MAD.Plugin.Mixfile do
 
   defp dialyzer() do
     opts = [
-      flags: [:error_handling],
-      plt_add_apps: [:syntax_tools]
+      flags: [:error_handling]
     ]
 
     if System.get_env("CI") == "true" do
@@ -76,9 +75,9 @@ defmodule Membrane.MP3.MAD.Plugin.Mixfile do
       {:unifex, "~> 1.1"},
       {:bundlex, "~> 1.3"},
       {:membrane_precompiled_dependency_provider, "~> 0.2.1"},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       # testing deps
       {:membrane_file_plugin, "~> 0.17.0", only: :test}
     ]
