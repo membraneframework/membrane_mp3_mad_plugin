4c5a5cad981d6a7899b3ed2511528e53baa2b6ce
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 12:12:51 +0200

    Rename prepend_llms_links to append_llms_links

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Membrane.MP3.MAD.Plugin.Mixfile do
       homepage_url: "https://membraneframework.org",
       deps: deps(),
       dialyzer: dialyzer(),
-      aliases: [docs: ["docs", &prepend_llms_links/1]]
+      aliases: [docs: ["docs", &append_llms_links/1]]
     ]
   end
 
@@ -83,7 +83,7 @@ defmodule Membrane.MP3.MAD.Plugin.Mixfile do
     ]
   end
 
-  defp prepend_llms_links(_) do
+  defp append_llms_links(_args) do
     output_dir = docs()[:output] || "doc"
     path = Path.join(output_dir, "llms.txt")
 
